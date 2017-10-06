33dd51b74f82110a02d94e81efd687f2e1a4982a
willnet <netwillnet@gmail.com>
2017-10-06 10:34:18 +0900

    Fix raise error on test due to bug of rails generator

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,7 @@
 require File.expand_path("../../test/dummy/config/environment.rb", __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
 require "rails/test_help"
+require "rails/test_unit/reporter"
 
 # Filter out Minitest backtrace while allowing backtrace from other libraries
 # to be shown.
