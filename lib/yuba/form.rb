982b2a2e1940ee44afb44b95881457baeb148a07
willnet <netwillnet@gmail.com>
2017-10-09 17:11:23 +0900

    Enable multi parameter assignment

--- lib/yuba/form.rb
+++ lib/yuba/form.rb
@@ -1,10 +1,12 @@
 require 'yuba/form/coercion'
+require 'yuba/form/multi_parameter_attributes'
 
 module Yuba
   class Form < ::Reform::Form
     class << self
       def inherited(subclass)
         subclass.feature Coercion
+        subclass.feature MultiParameterAttributes
       end
     end
   end
--- test/yuba/form_test.rb
+++ test/yuba/form_test.rb
@@ -1,21 +1,28 @@
 class Yuba::Form::Test < ActiveSupport::TestCase
+  form_class = Class.new(Yuba::Form) do
+    property :number, type: :int
+    property :start_time, type: :date_time
+  end
+
+  model_class = Class.new do
+    include ActiveModel::Model
+    attr_accessor :start_time, :number
+  end
+
   test 'create subclass' do
     Class.new(Yuba::Form)
   end
 
   test 'property works' do
-    klass = Class.new(Yuba::Form) do
-      property :number, type: :int
-    end
+    form = form_class.new(model_class.new)
+    form.number = '1'
+    assert_equal form.number, 1
+  end
 
-    model_class = Class.new do
-      include ActiveModel::Model
-
-      attr_accessor :number
-    end
-
-    object = klass.new(model_class.new)
-    object.number = '1'
-    assert_equal object.number, 1
+  test "munges multi-param date and time fields into a valid Time attribute" do
+    start_time_params = { "start_time(1i)"=>"2000", "start_time(2i)"=>"1", "start_time(3i)"=>"1", "start_time(4i)"=>"12", "start_time(5i)"=>"00" }
+    form = form_class.new(model_class.new)
+    form.validate(start_time_params)
+    assert_equal form.start_time, Time.zone.local(2000, 1, 1, 12, 0)
   end
 end
