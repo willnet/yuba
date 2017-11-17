f3291a28c58834ebd8fa19cce6a740acdf481ef6
willnet <netwillnet@gmail.com>
2017-11-17 18:57:23 +0900

    ViewModel also has property like Service

--- lib/yuba/view_model.rb
+++ lib/yuba/view_model.rb
@@ -1,7 +1,26 @@
 module Yuba
   class ViewModel
+    class_attribute :_properties
+    self._properties = {}
+
+    class << self
+      def property(name, options = {})
+        _properties[name.to_sym] = options
+      end
+    end
+
     def initialize(**args)
-      args.each { |k,v| self.singleton_class.send(:define_method, k) { v } }
+      args.keys.each do |key|
+        if !_properties.has_key?(key.to_sym) && !_properties.dig(key.to_sym, :optional)
+          raise ArgumentError, "missing 'property :#{key}' in #{self.class.name} class"
+        end
+      end
+
+      args.each do |key, value|
+        define_singleton_method key do
+          value
+        end
+      end
     end
   end
 end
--- test/yuba/view_model_test.rb
+++ test/yuba/view_model_test.rb
@@ -1,2 +1,11 @@
 class Yuba::ViewModel::Test < ActiveSupport::TestCase
+  view_model_class = Class.new(Yuba::Service) do
+    property :name
+    property :address, optional: true
+  end
+
+  test 'property works' do
+    view_model = view_model_class.new(name: 'willnet')
+    assert_equal view_model.name, 'willnet'
+  end
 end
