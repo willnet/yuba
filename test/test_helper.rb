9dc5e92a973fa4b4f74f644eba38d2f260954d1a
willnet <netwillnet@gmail.com>
2017-11-29 11:04:11 +0900

    Fix CI(maybe)

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ require File.expand_path("../../test/dummy/config/environment.rb", __FILE__)
 ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
 require "rails/test_help"
 require "rails/test_unit/reporter"
-
+require "selenium-webdriver"
 # Filter out Minitest backtrace while allowing backtrace from other libraries
 # to be shown.
 Minitest.backtrace_filter = Minitest::BacktraceFilter.new
