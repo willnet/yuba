46b7cbcdcd2dcca421646917b1aab2d857a854e7
willnet <netwillnet@gmail.com>
2017-12-11 00:12:18 +0900

    Add test to make sure that render works without view_model option

--- test/dummy/test/system/create_artists_test.rb
+++ test/dummy/test/system/create_artists_test.rb
@@ -1,7 +1,13 @@
 require "application_system_test_case"
 
 class CreateArtistsTest < ApplicationSystemTestCase
-  test "visiting the new" do
+  test 'visiting #index not using view_model option to render' do
+    visit artists_url
+
+    assert_selector 'h2', text: 'Artists Index'
+  end
+
+  test 'visiting #new using view_model option to render' do
     visit new_artist_url
 
     assert_selector 'h2', text: 'New Artist'
