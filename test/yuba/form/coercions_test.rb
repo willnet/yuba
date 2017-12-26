266d275b49fdcf23d42fd93e4507e3dc7f9072e4
willnet <netwillnet@gmail.com>
2017-12-27 00:01:53 +0900

    Add coercion tests (wip)

---
+++ test/yuba/form/coercions_test.rb
@@ -0,0 +1,32 @@
+require 'test_helper'
+
+class Yuba::Form::Coercions::Test < ActiveSupport::TestCase
+  form_class = Class.new(Yuba::Form) do
+    attribute :default
+    attribute :integer, type: :int
+    attribute :string, type: :string
+    attribute :date, type: :date
+    attribute :datetime, type: :datetime
+    attribute :boolean, type: :boolean
+    attribute :float, type: :float
+    attribute :decimal, type: :decimal
+    attribute :array, type: :array
+    attribute :hash, type: :hash
+  end
+
+  model_class = Class.new do
+    include ActiveModel::Model
+  end
+
+  test 'it works' do
+    form = form_class.new(model: model_class.new)
+    form.integer = '1'
+    assert_equal 1, form.integer
+    form.string = 1
+    assert_equal '1', form.string
+    form.date = '2017/12/31'
+    assert_equal Date.new(2017, 12, 31), form.date
+    form.datetime = '2017/12/31 12:34:56'
+    assert_equal Time.zone.local(2017, 12, 31, 12, 34, 56), form.datetime
+  end
+end
