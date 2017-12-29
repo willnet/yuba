1a61d6083cf240a095ef67a9d41c936058345adf
willnet <netwillnet@gmail.com>
2017-12-29 22:45:49 +0900

    Enable coercions

--- lib/yuba/form/coercions.rb
+++ lib/yuba/form/coercions.rb
@@ -2,10 +2,28 @@ module Yuba
   class Form
     module Coercions
       class << self
+        FALSE_VALUES = [false, 0, '0', 'f', 'F', 'false', 'FALSE', 'off', 'OFF'].to_set
+
         def coerce(type:, value:)
           case type
           when :int
             value.present? ? value.to_i : nil
+          when :string
+            value.to_s
+          when :date
+            Date.parse(value)
+          when :datetime
+            Time.zone.parse(value)
+          when :boolean
+            !FALSE_VALUES.include?(value)
+          when :float
+            value.to_f
+          when :decimal
+            BigDecimal.new(value)
+          when :array
+            value.empty? ? [] : value
+          when :hash
+            value.empty? ? {} : value
           else
             value
           end
--- test/yuba/form/coercions_test.rb
+++ test/yuba/form/coercions_test.rb
@@ -28,5 +28,21 @@ class Yuba::Form::Coercions::Test < ActiveSupport::TestCase
     assert_equal Date.new(2017, 12, 31), form.date
     form.datetime = '2017/12/31 12:34:56'
     assert_equal Time.zone.local(2017, 12, 31, 12, 34, 56), form.datetime
+    [false, 0, '0', 'f', 'F', 'false', 'FALSE', 'off', 'OFF'].each do |falsy|
+      form.boolean = falsy
+      refute form.boolean
+    end
+    [1, '1', 't', 'hoge'].each do |truthy|
+      form.boolean = truthy
+      assert form.boolean
+    end
+    form.float = '3.5'
+    assert 3.5, form.float
+    form.decimal = '123'
+    assert BigDecimal.new(123), form
+    form.array = ''
+    assert Array.new, form.array
+    form.hash = ''
+    assert Hash.new, form.hash
   end
 end
