aa9f5628d76c20896b53f2f2f6075619779befe9
willnet <netwillnet@gmail.com>
2018-01-01 09:50:16 +0900

    Enable assign multi parameter

--- lib/yuba/form.rb
+++ lib/yuba/form.rb
@@ -1,6 +1,7 @@
 require 'yuba'
 require 'yuba/form/attributes'
 require 'yuba/form/coercions'
+require 'yuba/form/multi_parameter_attributes'
 require 'active_model/naming'
 
 module Yuba
@@ -19,6 +20,17 @@ module Yuba
 
     def to_model
       @_model.to_model
+    end
+
+    def assign_attributes(attributes)
+      multi_parameter_attributes = MultiParameterAttributes.call(attributes)
+      multi_parameter_attributes.each do |k, v|
+        public_send("#{k}=", v)
+      end
+    end
+
+    def attributes=(attributes)
+      assign_attributes(attributes)
     end
   end
 end
--- test/yuba/form_test.rb
+++ test/yuba/form_test.rb
@@ -96,19 +96,29 @@ class Yuba::Form::Test < ActiveSupport::TestCase
     assert form.valid?
   end
 
-=begin
+  test '#assign_attributes works' do
+    form1 = simple_form_class.new(model: model_class.new)
+    form1.assign_attributes(number: '1')
+    assert_equal 1, form1.number
+  end
+
+  test '#attributes= works' do
+    form1 = simple_form_class.new(model: model_class.new)
+    form1.attributes= { number: '1' }
+    assert_equal 1, form1.number
+  end
+
   test "munges multi-param date and time fields into a valid Time attribute" do
     start_time_params = { "start_time(1i)"=>"2000", "start_time(2i)"=>"1", "start_time(3i)"=>"1", "start_time(4i)"=>"12", "start_time(5i)"=>"00" }
-    form = form_class.new(model: model_class.new)
-    form.validate(start_time_params)
-    assert_equal form.start_time, Time.zone.local(2000, 1, 1, 12, 0)
+    form = simple_form_class.new(model: model_class.new)
+    form.assign_attributes(start_time_params)
+    assert_equal Time.zone.local(2000, 1, 1, 12, 0), form.start_time
   end
 
   test "munges multi-param date and date fields into a valid Date attribute" do
     start_time_params = { "start_time(1i)"=>"2000", "start_time(2i)"=>"1", "start_time(3i)"=>"1" }
-    form = form_class.new(model: model_class.new)
-    form.validate(start_time_params)
-    assert_equal form.start_time, Time.zone.local(2000, 1, 1).to_date
+    form = simple_form_class.new(model: model_class.new)
+    form.assign_attributes(start_time_params)
+    assert_equal Time.zone.local(2000, 1, 1).to_date, form.start_time
   end
-=end
 end
