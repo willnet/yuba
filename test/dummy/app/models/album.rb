9af926510a48c5003880dd1f7ae8311cfeb2a098
willnet <netwillnet@gmail.com>
2018-01-06 16:38:26 +0900

    Remove system tests
    it’s difficult to use AR with both of unit test and system test. and it’s enough to test without system test.

--- test/dummy/app/models/album.rb
+++
@@ -1,3 +0,0 @@
-class Album < ApplicationRecord
-  belongs_to :artist
-end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,20 +1,11 @@
-require File.expand_path("../../test/dummy/config/environment.rb", __FILE__)
-ActiveRecord::Migrator.migrations_paths = [File.expand_path("../../test/dummy/db/migrate", __FILE__)]
-require "rails/test_help"
-require "rails/test_unit/reporter"
-require "selenium-webdriver"
-require 'webdrivers/chromedriver'
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 
-# Filter out Minitest backtrace while allowing backtrace from other libraries
-# to be shown.
-Minitest.backtrace_filter = Minitest::BacktraceFilter.new
-
-Rails::TestUnitReporter.executable = 'bin/test'
-
-# Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("../fixtures", __FILE__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
-  ActiveSupport::TestCase.fixtures :all
-end
+require "rails"
+require "active_support/all"
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "active_record/railtie"
+require "yuba"
+Time.zone = 'Tokyo'
+require "minitest/autorun"
